0a1b5efc98390f30e2ef8892081cdbca2b918896
Daniel Mikhalchuk <zedudedaniel@gmail.com>
2019-10-03 14:47:31 -0400

    Solution to Curiosity Rover Exercise

---
+++ lab02/rover.py
@@ -0,0 +1,12 @@
+#Using 'python3 rover.py' didn't work, said python3 is not recognized
+#Also already did all the pycodestyle download stuff
+
+def main():
+    print("How long does it take a photo to reach NASA from Curiosity?")
+    print("This takes light speed, which is 186,000 miles a second.")
+    print("The closest distance from Curiosity to NASA is 34 million miles.")
+    time = (34000000/186000)
+    print("The time it takes is roughly " + time + " seconds.")
+
+
+main()
