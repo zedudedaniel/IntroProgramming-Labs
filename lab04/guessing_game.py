76e56e5ec024becf5c309ef962c38268e2331cac
Daniel Mikhalchuk <zedudedaniel@gmail.com>
2019-10-10 14:37:20 -0400

    Amend not working?

--- lab04/guessing_game.py
+++ lab04/guessing_game.py
@@ -2,11 +2,14 @@
 #Keep repeating until they get it correct
 
 def main():
-    animal = "Tardigrade"
+    animal = "tardigrade"
     while (True):
-        guess = input("Guess the animal! ")
+        guess = input("Guess the animal! ").lower()
         if (guess == animal):
             print("Correct!")
+            break
+        elif (guess == "quit"):
+            print("Seeya!")
             break
         else:
             print("Incorrect! Try again.")
