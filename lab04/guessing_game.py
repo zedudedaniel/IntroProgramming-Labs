76f16d6a95fe996734d8b73a4ac1c5047352234b
Daniel Mikhalchuk <zedudedaniel@gmail.com>
2019-10-10 14:51:04 -0400

    Added the y/n and q quitting instantly

--- lab04/guessing_game.py
+++ lab04/guessing_game.py
@@ -5,11 +5,16 @@ def main():
     animal = "tardigrade"
     while (True):
         guess = input("Guess the animal! ").lower()
-        if (guess == animal):
-            print("Correct!")
-            break
-        elif (guess == "quit"):
+        if (guess[0] == "q"):
             print("Seeya!")
+            break
+        elif (guess == animal):
+            print("Correct!")
+            likeanimal = input("Do you like them? y/n ").lower()
+            if (likeanimal == "y"):
+                print("I see you are a man of culture as well.")
+            else:
+                print("Thomas has never seen such bullshit before.")
             break
         else:
             print("Incorrect! Try again.")
