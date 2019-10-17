952983dc2da8e6ef6700bc025102ccf7dcda3d08
Daniel Mikhalchuk <zedudedaniel@gmail.com>
2019-10-17 14:21:07 -0400

    Second changes

--- lab05/credentials.py
+++ lab05/credentials.py
@@ -6,20 +6,31 @@
 # I remember doing this one but I guess git push failed :(
 
 def main():
+	names = getNames()
+
+	uname = makeName(names)
+	passwd = makePass()
+	print("Account made. Your new email address is", uname+"@marist.edu")
+	
+
+def getNames():
 	#get user's first and last names
 	first = input("Enter your first name: ")
 	last = input("Enter your last name: ")
+	names = [first,last]
+	return names
 
-	#Make a marist-style username for them
-	uname = first + "." + last
+def makeName(names):
+	return names[0] + "." + names[1]
 
+def makePass():
 	#ask user for password
-	passwd = input("Make a password for your email.")
+	passwd = input("Make a password for your email. ")
 
 	while (len(passwd) < 8):
-		passwd = input("You need a longer password! 8 characters at least.")
-	print("That's a good password")
+		passwd = input("You need a longer password! 8 characters at least. ")
+	print("That's a good password.")
 
-	print("Account made. Your new email address is", uname+"@marist.edu")
+	return passwd
 
 main()
