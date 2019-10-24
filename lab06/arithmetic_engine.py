b2fef5caca127cdfb356e4c3e79249086baf1a9a
Daniel Mikhalchuk <zedudedaniel@gmail.com>
2019-10-24 14:16:11 -0400

    First step

---
+++ lab06/arithmetic_engine.py
@@ -0,0 +1,46 @@
+#CMPT 120 - Lab 06
+#Daniel Mikhalchuk
+# 10-24-2019
+
+def show_intro():
+	print("Welcome to the Arithmetic engine!")
+	print("==========================\n")
+	print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+def show_outro():
+	print("\nThank you for using the Arithmetic Engine!")
+	print("\nPlease come back again soon!")
+
+def do_loop():
+	while True:
+		cmd = (input("What computation do you want to perform?: ")).lower()
+		
+
+		if cmd == "quit":
+			break
+		elif (cmd != "add" and cmd != "sub" and cmd!= "mult" and cmd != "div"):
+			print(cmd + " is not a valid command.")
+			continue
+
+		num1 = int(input("Enter the first number: "))
+		num2 = int(input("Enter the second number: "))
+
+		result = calculate(num1, cmd, num2)
+		print("The result is: " + str(result) + "\n")
+
+def calculate(num1, cmd, num2):
+	if cmd == "add":
+		return (num1 + num2)
+	elif cmd == "sub":
+		return (num1 - num2)
+	elif cmd == "mult":
+		return (num1 * num2)
+	elif cmd == "div":
+		return (num1 / num2)
+
+def main():
+	show_intro()
+	do_loop()
+	show_outro()
+
+main()
